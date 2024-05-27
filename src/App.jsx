dc18888cd5febac36574785c5384c76e7c84b214
Vasyl1yyy <115637018+Vasyl1yyy@users.noreply.github.com>
2024-05-27 12:48:13 +0300

    open

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="flex bg-main text-white h-screen justify-center items-center font-fontMain"></div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    colors: {
+      main: '#1A191A',
+      gray: '#242324',
+      grayWhite: '#4b4b4b',
+      white: '#FEFFFE',
+      purple: '#7332C3',
+      purpleWhite: '#bd59ff',
+      green: '#7BF42F',
+      red: '#F13A3A',
+      yellow: '#F9E828',
+      blue: '#36A2F4',
+    },
+    fontFamily: {
+      fontMain: ['Nunito', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
