a6eb57ac771149b8158a29e861051bb7fecabcb4
Vasyl1yyy <115637018+Vasyl1yyy@users.noreply.github.com>
2024-05-27 14:54:22 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import LogIn from './components/Sign/LogIn/LogIn';
 import SignUp from './components/Sign/SignUp/SIgnUp';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="bg-main text-white h-screen font-fontMain">
-      <div className="container mx-auto h-screen flex justify-center items-center">
-        <Routes>
-          <Route path="log-in" element={<LogIn />} />
-          <Route path="sign-up" element={<SignUp />} />
-        </Routes>
+      <div className="container mx-auto h-screen pt-2">
+        <Header />
+        <div className="flex justify-center item-center">
+          <Routes>
+            <Route path="log-in" element={<LogIn />} />
+            <Route path="sign-up" element={<SignUp />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,36 @@
+import { Link, NavLink } from 'react-router-dom';
+
 function Header() {
-  return;
+  return (
+    <div className="mx-3 px-10 py-2 flex justify-between items-center bg-gray rounded-full transition-colors border-grayWhite hover:border-purple border-2 mb-12">
+      <div className="text-lg font-extrabold text-purpleWhite">Logo</div>
+      <ul className="flex items-center gap-6 font-bold text-sm">
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/task">Task</NavLink>
+        </li>
+        <li>
+          <NavLink to="/habit">Habit</NavLink>
+        </li>
+        <li>
+          <NavLink to="/stats">Stats</NavLink>
+        </li>
+      </ul>
+      <div className="flex text-sm font-bold gap-3 justify-center items-center">
+        <Link
+          className="text-white bg-purple rounded-full px-3 py-1.5"
+          to="/sign-up"
+        >
+          Sign up
+        </Link>
+        <Link className=" text-purpleWhite" to="/log-in">
+          Log in
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
